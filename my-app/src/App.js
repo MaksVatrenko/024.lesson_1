97d3815048a535949584c35b8acd37c7228e227d
maksVatrenko <maks1004200097@gmail.com>
2024-01-10 17:28:35 +0200

    First React App

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App__header">
+          header
+      </header>
+    <nav className="App__nav">
+        <ul>
+            <li><a href="">Nav 1</a></li>
+            <li><a href="">Nav 2</a></li>
+            <li><a href="">Nav 3</a></li>
+        </ul>
+    </nav>
+      <main class="App__main">
+          main
+      </main>
+      <footer class="App__footer">
+          footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
